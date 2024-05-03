251a4ff65627f9f247fbc287aaeed4d36e49e2cd
Ahmed Sobh <sobh7771@gmail.com>
2024-05-04 01:58:07 +0400

    Resolved all build errors

--- src/compnants/Contact1.js
+++ src/compnants/Contact1.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-export default function () {
+function Contact() {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 '>
-    <form action='https://getform.io/f/nelJq5bK'method='post' className='flex flex-col max-w-[600px] w-full'>
-        <div className='pb-8'>
-            <p className='font-bold inline text-gray-300 border-b-4 border-b-pink-600 text-4xl '>Contact</p>
-            <p className='text-gray-300 py-4'>// Submit the form below me an email - hessendawood46@gmail.com</p>
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 ">
+      <form
+        action="https://getform.io/f/nelJq5bK"
+        method="post"
+        className="flex flex-col max-w-[600px] w-full">
+        <div className="pb-8">
+          <p className="font-bold inline text-gray-300 border-b-4 border-b-pink-600 text-4xl ">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            Submit the form below me an email - hessendawood46@gmail.com
+          </p>
         </div>
-        <input className=' rounded bg-[#ccd5f6] p-2'type='text' placeholder='name ' name='name'/>
-        <input className='my-4 p-2 rounded  bg-[#ccd5f6]' type='email' placeholder='Email ' name='email'/>
-        <textarea className='bg-[#ccd5f6] rounded p-2'  placeholder='massage' name='massage' cols='30' rows='10'/>
-        <button className='border-2 text-gray-300 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center  hover:bg-pink-600 duration-300 '>let's Collaborate </button>
-    </form>
-
-</div>
-  )
+        <input className=" rounded bg-[#ccd5f6] p-2" type="text" placeholder="name " name="name" />
+        <input
+          className="my-4 p-2 rounded  bg-[#ccd5f6]"
+          type="email"
+          placeholder="Email "
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd5f6] rounded p-2"
+          placeholder="massage"
+          name="massage"
+          cols="30"
+          rows="10"
+        />
+        <button className="border-2 text-gray-300 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center  hover:bg-pink-600 duration-300 ">
+          let's Collaborate{' '}
+        </button>
+      </form>
+    </div>
+  );
 }
+
+export default Contact;
--- src/compnants/Navbar.js
+++ src/compnants/Navbar.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import Logo from "../assets/logo-no-background.png";
-import { Link } from "react-scroll";
+import React, { useState } from 'react';
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Logo from '../assets/logo-no-background.png';
+import { Link } from 'react-scroll';
 export default function Navbar() {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300  ">
       <div>
-        <img className="pt-5 " src={Logo} alt="logo img" style={{ width: "200px" }} />
+        <img className="pt-5 " src={Logo} alt="logo img" style={{ width: '200px' }} />
       </div>
 
       {/* menu */}
@@ -26,7 +26,7 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link  className=" font-bold font-serif " to="skills" smooth={true} duration={500}>
+          <Link className=" font-bold font-serif " to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
@@ -51,10 +51,9 @@ export default function Navbar() {
       <ul
         className={
           !nav
-            ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
-        }
-      >
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }>
         <li className="py-6 text-4xl">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
@@ -87,34 +86,26 @@ export default function Navbar() {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500 rounded">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.linkedin.com/in/hussein-dawood-446aa6263?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-            >
-              Linkedin <FaLinkedin  size={30} />
+              href="https://www.linkedin.com/in/hussein-dawood-446aa6263?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+              Linkedin <FaLinkedin size={30} />
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-zinc-800 rounded">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://github.com/husseindawood46"
-            >
-            
+              href="https://github.com/husseindawood46">
               GitHup <FaGithub size={30} />
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-emerald-400 rounded">
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href=""
-            >
-              {" "}
+            {/* I have no idea what the href will be; adjust it */}
+            <a className="flex justify-between items-center w-full text-gray-300" href="/">
+              {' '}
               Email <HiOutlineMail size={30} />
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-700 rounded">
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href=""
-            >
+            <a className="flex justify-between items-center w-full text-gray-300" href="/">
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
